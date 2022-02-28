e942ec519482aaec5a47a7a536cb9dd9769c74d1
Daniel Chistyakov <4i.danila@gmail.com>
2022-02-28 23:08:46 +0300

    War alert added

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,21 +1,21 @@
-import { FC, useState } from "react";
-import { useRouter } from "next/router";
-import Link from "next/link";
-import styles from "../styles/Header.module.scss";
-import Icons from "../images/Icons";
-import CabinetMenu from "./Cabinet/CabinetMenu";
-import Menu from "./Menu";
-import AuthPopup from "./Cabinet/AuthPopup";
+import { FC, useState } from 'react';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import styles from '../styles/Header.module.scss';
+import Icons from '../images/Icons';
+import CabinetMenu from './Cabinet/CabinetMenu';
+import Menu from './Menu';
+import AuthPopup from './Cabinet/AuthPopup';
 
 const Header: FC = () => {
   const nav = [
-    { title: "Сейчас смотрят", href: "/category/watching" },
-    { title: "Новинки", href: "/category/last" },
-    { title: "Фильмы", href: "/category/films" },
-    { title: "Сериалы", href: "/category/series" },
-    { title: "Мультфильмы", href: "/category/cartoons" },
-    { title: "Телепередачи", href: "/category/show" },
-    { title: "Аниме", href: "/category/animation" },
+    { title: 'Сейчас смотрят', href: '/category/watching' },
+    { title: 'Новинки', href: '/category/last' },
+    { title: 'Фильмы', href: '/category/films' },
+    { title: 'Сериалы', href: '/category/series' },
+    { title: 'Мультфильмы', href: '/category/cartoons' },
+    { title: 'Телепередачи', href: '/category/show' },
+    { title: 'Аниме', href: '/category/animation' },
   ];
 
   const [opened, setOpen] = useState<boolean>(false);
@@ -26,35 +26,27 @@ const Header: FC = () => {
       {authPopup && <AuthPopup setAuthPopup={setAuthPopup} />}
       <>
         {!opened ? (
-          <Icons
-            icon="MenuIcon"
-            onClick={() => setOpen(true)}
-            className={styles.menu_icon}
-          />
+          <Icons icon='MenuIcon' onClick={() => setOpen(true)} className={styles.menu_icon} />
         ) : (
-          <Icons
-            icon="CloseIcon"
-            onClick={() => setOpen(false)}
-            className={styles.menu_icon}
-          />
+          <Icons icon='CloseIcon' onClick={() => setOpen(false)} className={styles.menu_icon} />
         )}
         {opened && <Menu setOpen={setOpen} />}
-        <Link href={"/"}>
+        <Link href={'/'}>
           <a className={styles.logo}>Дядька в кино</a>
         </Link>
         <div className={styles.icons}>
-          <Link href="/search">
+          <Link href='/search'>
             <a className={styles.icon}>
-              <Icons icon="SearchIcon" />
+              <Icons icon='SearchIcon' />
             </a>
           </Link>
-          <Link href="/favorites">
+          <Link href='/favorites'>
             <a className={styles.icon}>
-              <Icons icon="BookmarkIcon" />
+              <Icons icon='BookmarkIcon' />
             </a>
           </Link>
           <span className={styles.icon} onClick={() => setAuthPopup(true)}>
-            <Icons icon="PersonIcon" />
+            <Icons icon='PersonIcon' />
           </span>
         </div>
       </>
@@ -63,11 +55,7 @@ const Header: FC = () => {
           {nav?.map(({ href, title }, key) => (
             <div key={key}>
               <Link href={href} passHref>
-                <a
-                  className={`${styles.item}${
-                    asPath === href ? ` ${styles.item_active}` : ""
-                  }`}
-                >
+                <a className={`${styles.item}${asPath === href ? ` ${styles.item_active}` : ''}`}>
                   {title}
                 </a>
               </Link>
@@ -75,17 +63,13 @@ const Header: FC = () => {
           ))}
         </div>
       </nav>
-      <Link href="/search">
+      <Link href='/search'>
         <a className={styles.search_icon}>
-          <Icons icon="SearchIcon" />
+          <Icons icon='SearchIcon' />
         </a>
       </Link>
-      <Link href="/favorites">
-        <a
-          className={`${styles.item}${
-            asPath === "/favorites" ? ` ${styles.item_active}` : ""
-          }`}
-        >
+      <Link href='/favorites'>
+        <a className={`${styles.item}${asPath === '/favorites' ? ` ${styles.item_active}` : ''}`}>
           Избранное
         </a>
       </Link>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
-import {FC} from 'react';
-import {AppProps} from 'next/app';
+import { FC } from 'react';
+import { AppProps } from 'next/app';
 import '../models/init';
 import '../styles/App.sass';
 import Header from '../components/Header';
@@ -13,21 +13,29 @@ import 'nprogress/nprogress.css'; //styles of nprogress
 import Router from 'next/router';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 import Link from 'next/link';
-import {Provider} from 'effector-react/ssr';
-import {fork} from 'effector';
-import {root} from '@models/Root';
+import { Provider } from 'effector-react/ssr';
+import { fork } from 'effector';
+import { root } from '@models/Root';
 
 export const isBrowser = () => typeof window !== 'undefined';
 
-const MyApp: FC<AppProps & { err: any }> = ({Component, pageProps, err}) => {
+const MyApp: FC<AppProps & { err: any }> = ({ Component, pageProps, err }) => {
   Router.events.on('routeChangeStart', () => NProgress.start());
   Router.events.on('routeChangeComplete', () => NProgress.done());
   Router.events.on('routeChangeError', () => NProgress.done());
 
-  const scope = fork(root, {values: pageProps.store});
+  const scope = fork(root, { values: pageProps.store });
 
   return (
     <Provider value={scope}>
+      <a
+        href='https://www.instagram.com/explore/tags/%D0%BD%D0%B5%D1%82%D0%B2%D0%BE%D0%B9%D0%BD%D0%B5/'
+        target='_blank'
+        rel='noreferrer'
+        className='war'
+      >
+        #НЕТВОЙНЕ
+      </a>
       <Header />
       <Component {...pageProps} err={err} />
       <Footer />
